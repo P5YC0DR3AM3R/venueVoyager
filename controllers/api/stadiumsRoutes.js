c7b4074fbcd0c46aff8e562c21401506d33f8c59
Micah Read <164657374+P5YC0DR3AM3R@users.noreply.github.com>
2024-05-14 15:59:45 -0400

    functional server running node server.js after postgres:\i schema.sql

--- controllers/api/stadiumsRoutes.js
+++ controllers/api/stadiumsRoutes.js
@@ -4,17 +4,13 @@ const { getStadiumImages } = require('../../utils/wiki');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
     const stadiumData = await Stadium.findAll({
       attributes: ['stadium_id', 'stadium', 'team', 'league', 'division', 'city', 'state'],
     });
 
-    // Serialize data so the template can read it
     const stadiums = stadiumData.map((stadium) => stadium.get({ plain: true }));
 
-    // Send the serialized data as JSON
     res.json(stadiums);
-
   } catch (err) {
     res.status(500).json(err);
   }
@@ -22,17 +18,13 @@ router.get('/', async (req, res) => {
 
 router.get('/stadium/:id', async (req, res) => {
   try {
-    // Find a single stadium by its ID
     const stadiumData = await Stadium.findByPk(req.params.id, {
       attributes: ['stadium_id', 'stadium', 'team', 'league', 'division', 'city', 'state'],
     });
 
-    // Serialize the data so the template can read it
     const stadium = stadiumData.get({ plain: true });
 
-    // Send the serialized data as JSON
     res.json(stadium);
-
   } catch (err) {
     res.status(500).json(err);
   }
@@ -61,4 +53,3 @@ router.get('/stadium-images/:name', async (req, res) => {
 });
 
 module.exports = router;
-
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-const sequelize = require('./config/connnection');
+const sequelize = require('./config/connection');
 // Create a new sequelize store using the express-session package
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
