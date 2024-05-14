b4562a840130eabdefb8b60d4be23ecb6c5a736f
Micah Read <164657374+P5YC0DR3AM3R@users.noreply.github.com>
2024-05-14 11:50:18 -0400

    stadium instead of project

--- models/Stadiums.js
+++ models/Stadiums.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Project extends Model {}
+class Stadiums extends Model {}
 
-Project.init(
+Stadiums.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -44,4 +44,4 @@ Project.init(
   }
 );
 
-module.exports = Project;
+module.exports = Stadiums;
