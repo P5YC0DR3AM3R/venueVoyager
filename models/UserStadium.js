14dc8e972575a69981ca556053918c3196177f9f
RyanH118 <164549773+RyanH118@users.noreply.github.com>
2024-05-14 19:52:38 -0400

    Revert "broken, location column doesnt exist in stadium table"

--- models/UserStadium.js
+++ models/UserStadium.js
@@ -4,44 +4,42 @@ const sequelize = require('../config/connection');
 class UserStadium extends Model {}
 
 UserStadium.init(
-  {
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-      onDelete: 'CASCADE'
-    },
-    stadium_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'stadium',
-        key: 'id',
-      },
-      onDelete: 'CASCADE'
-    },
-    date_visited: {
-      type: DataTypes.DATE
-    },
-    rating: {
-      type: DataTypes.INTEGER,
-      validate: {
-        min: 1,
-        max: 5
-      }
-    },
-    review: {
-      type: DataTypes.STRING
-    },
-  },
-  {
-    sequelize,
-    timestamps: true,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user_stadium',
-  }
+    {
+        date_visited: {
+            type: DataTypes.DATE,
+          },
+          rating: {
+            type: DataTypes.INTEGER,
+            validate: {
+              min: 1,
+              max: 5, // Adjust validation range as needed
+            },
+          },
+          review: {
+            type: DataTypes.STRING,
+          },
+          user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id',
+            },
+          },
+          stadium_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'stadium',
+                key: 'id',
+              },
+          },
+    }, 
+    {
+        sequelize,
+        timestamps: false, 
+        freezeTableName: true, 
+        underscored: true,
+        modelName: 'user_stadiums',
+    }
 );
 
 module.exports = UserStadium;
