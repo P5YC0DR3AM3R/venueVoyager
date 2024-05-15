e288c4de8a61b9ccc523dfef946d3e78386191e8
dmharrison <dmharrison2014@gmail.com>
2024-05-15 10:58:59 -0400

    home page cards

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
+const { Stadium, User } = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
@@ -25,10 +25,9 @@ router.get("/", async (req, res) => {
       logo: "https://1000logos.net/wp-content/uploads/2017/05/NHL-Logo-500x333.png",
     },
   ];
-  res.render("homepage", { cardData });
+  res.render("homepage", { cardData, logged_in: req.session.logged_in });
 });
 
-// Route to handle stadium listing for a specific sport (placeholder)
 router.get("/:sport/stadiums", withAuth, async (req, res) => {
   const sport = req.params.sport;
   // Replace this with your logic to fetch and display stadiums associated with the sport
@@ -37,7 +36,27 @@ router.get("/:sport/stadiums", withAuth, async (req, res) => {
   res.render("stadiums", { sport, stadiums }); // Pass sport and stadiums data to the template
 });
 
-router.get("/login", withAuth, async (req, res) => {
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Stadium }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// Route to handle stadium listing for a specific sport (placeholder)
+
+router.get("/login", async (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect("/profile");
